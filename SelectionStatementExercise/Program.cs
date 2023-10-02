7063c89a4677a4e6c51d9cc3fcd1213a1d7cf1bb
sarajadidi <sararivkajadidi@gmail.com>
2023-10-02 11:04:38 -0700

    completed first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            //my number will be 222
+            var s = 222;
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+
+            if (userInput < s)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > s)
+            {
+                Console.WriteLine("Too High");
+            }
+            else 
+            {
+                Console.WriteLine("YES!");
+            }
+
+            //Console.WriteLine("Try again");
+            //var secondInput = int.Parse(Console.ReadLine());
+
+            //if (secondInput < s)
+            //{
+            //    Console.WriteLine("Too Low");
+            //}
+            //else if (secondInput > s)
+            //{
+            //    Console.WriteLine("Too High");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("YES!");
+            //}
         }
     }
 }
